49af55434d2cb084509184905ec07d013ff4fb1d
kite.lin <kite.lin@blackwellglobal.com>
2023-09-10 23:50:12 +0800

    add relation

--- Modules/Crawl/Entities/CrawledResult.php
+++ Modules/Crawl/Entities/CrawledResult.php
@@ -2,9 +2,11 @@
 
 namespace Modules\Crawl\Entities;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Crawl\Database\factories\CrawledResultFactory;
+use Modules\Document\Entities\Document;
 
 class CrawledResult extends Model
 {
@@ -12,11 +14,20 @@ class CrawledResult extends Model
     protected $table = 'crawled_results';
 
     protected $fillable = [
+        'user_id',
         'title',
         'url',
         'description',
         'body',
     ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function documents()
+    {
+        return $this->morphOne(Document::class, 'application');
+    }
 
     protected static function newFactory()
     {
