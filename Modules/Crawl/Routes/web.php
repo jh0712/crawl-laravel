e8fad416abdf7d302e9ea1b7686bff1358426469
kite.lin <kite.lin@blackwellglobal.com>
2023-09-10 21:17:45 +0800

    add test file and import screenshot plugin

--- Modules/Crawl/Routes/web.php
+++ Modules/Crawl/Routes/web.php
@@ -10,7 +10,18 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::prefix('crawl')->group(function() {
-    Route::get('/', 'CrawlController@index');
+use Modules\Crawl\Http\Controllers\CrawlTestController;
+//Route::prefix('crawl')->group(function() {
+//    Route::get('/', 'CrawlController@index');
+//});
+Route::group(['middleware' => ['web', 'auth']], function () {
+    // Payment Notify
+    Route::group([
+        'prefix' => 'crawl-management',
+        'as' => 'crawl-management.',
+    ], function () {
+        // crawl-management/crawl
+        // crawl-management.index
+        Route::get('/crawl', [CrawlTestController::class, 'index'])->name('index');
+    });
 });
